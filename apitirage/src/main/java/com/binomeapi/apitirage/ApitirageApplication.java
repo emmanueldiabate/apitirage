88aba81dee3f6ac7df423e1784b4dd57f79ce101
Emmanuel Diabaté <eddiabate@odc.local>
2022-08-18 09:57:57 +0000

    Api_tirage

---
+++ apitirage/src/main/java/com/binomeapi/apitirage/ApitirageApplication.java
@@ -0,0 +1,13 @@
+package com.binomeapi.apitirage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApitirageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApitirageApplication.class, args);
+	}
+
+}
---
+++ apitirage/src/test/java/com/binomeapi/apitirage/ApitirageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binomeapi.apitirage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApitirageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
