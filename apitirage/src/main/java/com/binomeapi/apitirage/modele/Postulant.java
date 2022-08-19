fd5e440002488b91247838aaf07914142b900f62
Kadidiatou SAMAKE <ksamake18@gmail.com>
2022-08-19 15:06:23 +0000

    les classe du modele

--- apitirage/src/main/java/com/binomeapi/apitirage/modele/Postulant.java
+++ apitirage/src/main/java/com/binomeapi/apitirage/modele/Postulant.java
@@ -3,15 +3,14 @@ package com.binomeapi.apitirage.modele;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "postulant")
-public class postulant {
+@Table(name = "POSTULANT")
+public class Postulant {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private Long id_post;
     private String nom;
     private String prenom;
     private String numero;
-    private String email;
+    private String mail;
 }
--- apitirage/src/main/java/com/binomeapi/apitirage/modele/Tirage.java
+++ apitirage/src/main/java/com/binomeapi/apitirage/modele/Tirage.java
@@ -1,17 +1,23 @@
 package com.binomeapi.apitirage.modele;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.sql.Date;
 
 @Entity
-@Table(name = "tirage")
-public class tirage {
+@Table(name = "TIRAGE")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Tirage {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private Long id_tirage;
     private String libelle;
     private Date date;
+    private Long nbPosTirer;
 }
